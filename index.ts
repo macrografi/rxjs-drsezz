db45dfa993ce039046791e9afc02bd40714a8555
macrografi <macrografi@hotmail.com>
2019-12-02 00:03:42 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -28,14 +28,25 @@ let allBooksObservable$ = new Observable(subscriber => {
 allBooksObservable$.subscribe(book => console.log(book.title));
 */
 
-/*OF*/
+/*
+//OF
 let source1$ = of("String", 10, true, allReaders[0].name);
 source1$.subscribe(value => console.log(value));
 
-/*FROM*/
+//FROM
 let source2$ = from(allBooks);
 source2$.subscribe(value => console.log(value.title));
 
-/*CONCAT*/
+//CONCAT
 concat(source1$,source2$).subscribe(value => console.log(value));
+*/
 
+let button = document.getElementById("readersButton");
+fromEvent(button, "click").subscribe(event => {
+  console.log(event);
+
+  let readersDiv = document.getElementById("readers");
+  for (let reader of allReaders) {
+    readersDiv.innerHTML += reader.name + "<br>";
+  }
+});
